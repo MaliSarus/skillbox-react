7a057c2c1ba1c0a50e164765627b0a99a1dad4a1
Alexandr Gabelko <43217693+MaliSarus@users.noreply.github.com>
2022-03-12 18:19:57 +0300

    try to add hrm

--- config/webpack.server.config.js
+++ config/webpack.server.config.js
@@ -2,8 +2,13 @@ const path = require('path')
 const NODE_ENV = process.env.NODE_ENV;
 const nodeExternals = require('webpack-node-externals')
 const NodemonPlugin = require('nodemon-webpack-plugin')
-
-module.exports = {
+const express = require('express')
+const hmrServer = express();
+const webpackDevMiddleWare = require('webpack-dev-middleware')
+const webpackHotMiddleWare = require('webpack-hot-middleware')
+const configures = require('../webpack.config')
+const webpack = require('webpack')
+const config = {
   target: "node",
   mode: NODE_ENV ? NODE_ENV : 'development',
   resolve: {
@@ -35,3 +40,21 @@ module.exports = {
     minimize: false
   }
 }
+
+const clientCompiler = webpack(config);
+
+hmrServer.use(webpackDevMiddleWare(configures.clientCompiler,{
+  publicPath: clientConfig.output.publicPath,
+  serverSideRender: true,
+  writeToDisk: true,
+  stats: 'errors-only',
+}))
+
+hmrServer.use(webpackHotMiddleWare(configures.clientCompiler, {
+  path: '/static/__webpack_hmr',
+}))
+
+hmrServer.listen(3001, () => {
+  console.log('HMR Started')
+})
+module.exports = config
