f39c71682510fbbc0b20f8f9b201a7ca70407d2a
Alexandr Gabelko <43217693+MaliSarus@users.noreply.github.com>
2022-03-13 10:47:12 +0300

    add ssr from lesson

--- config/webpack.server.config.js
+++ config/webpack.server.config.js
@@ -1,13 +1,8 @@
 const path = require('path')
 const NODE_ENV = process.env.NODE_ENV;
-const nodeExternals = require('webpack-node-externals')
-const NodemonPlugin = require('nodemon-webpack-plugin')
 const express = require('express')
-const hmrServer = express();
-const webpackDevMiddleWare = require('webpack-dev-middleware')
-const webpackHotMiddleWare = require('webpack-hot-middleware')
-const configures = require('../webpack.config')
 const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
 const config = {
   target: "node",
   mode: NODE_ENV ? NODE_ENV : 'development',
@@ -19,6 +14,9 @@ const config = {
     path: path.resolve(__dirname,'../dist/server'),
     filename: 'server.js'
   },
+  externals: [
+    nodeExternals()
+  ],
   module: {
     rules: [
       {
@@ -27,34 +25,9 @@ const config = {
       }
     ]
   },
-  watch: true,
-  plugins: [
-    new NodemonPlugin({
-      watch: [path.resolve(__dirname, '../dist/server'), path.resolve(__dirname, '../dist/client')]
-    }),
-  ],
-  externals: [
-    nodeExternals()
-  ],
   optimization: {
     minimize: false
   }
 }
 
-const clientCompiler = webpack(config);
-
-hmrServer.use(webpackDevMiddleWare(configures.clientCompiler,{
-  publicPath: clientConfig.output.publicPath,
-  serverSideRender: true,
-  writeToDisk: true,
-  stats: 'errors-only',
-}))
-
-hmrServer.use(webpackHotMiddleWare(configures.clientCompiler, {
-  path: '/static/__webpack_hmr',
-}))
-
-hmrServer.listen(3001, () => {
-  console.log('HMR Started')
-})
 module.exports = config
--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 export function Header(){
   return (
     <header>
-      <h1>Заголовок из  React1</h1>
+      <h1>Заголовок из  React12</h1>
     </header>
   )
 }
